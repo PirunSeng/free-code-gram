6e304e755ec305d8c82cc4c5aef26c88325589f1
PirunSeng <sengpirun.rain@gmail.com>
2020-04-10 10:15:56 +0700

    authenticate user on creating post and able to save post to db

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,16 +6,25 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+
+  public function __construct() {
+    $this->middleware('auth');
+  }
+
   public function create() {
     return view('posts.create');
   }
 
   public function store() {
     $data = request()->validate([
+      // 'notMandatoryField' => '' // field no validation, need to pass empty string
       'caption' => 'required',
       'image' => 'required|image',
-      // 'image' => ['required', 'image'],
+      // 'image' => ['required', 'image'], // alternative way
     ]);
+
+    auth()->user()->posts()->create($data);
+
     dd(request()->all());
   }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+  // allow sending any params/fields.
+  // To be checked how to allow only specific fields/params.
+  protected $guarded = [];
+
   public function user() {
     return $this->belongsTo(User::class);
   }
