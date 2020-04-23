447aa0c64f66d98b01bee370fa1d29992d0c0e72
PirunSeng <sengpirun.rain@gmail.com>
2020-04-23 14:57:09 +0700

    sends email with Mailtrap to new register user

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Welcome to FreeCodeGram
+
+This is the community of fellow developers and we love that you joined us.
+
+Thanks and regards,<br>
+Pirun
+@endcomponent
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Mail\NewUserWelcomeMail;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
+
+Route::get('/email', function() {
+  return new NewUserWelcomeMail();
+});
 
 Route::get('/profiles/{user}', 'ProfilesController@index')->name('profile.show');
 Route::get('/profiles/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
